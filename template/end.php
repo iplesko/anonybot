44a07ac19d35949a298cbb13bde307ca44798708
Ivan Pleško <plesko@essential-data.sk>
2017-12-28 15:47:10 +0100

    Add missing html title

--- template/end.php
+++ template/end.php
@@ -5,7 +5,7 @@
     <div class="col-sm-12">
         <hr />
         <p class="text-muted text-right">
-            v1.0.1,
+            v1.0.2,
             <a href="https://github.com/iplesko/anonybot">github</a>
         </p>
     </div>
